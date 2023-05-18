f420563f061c6f7fffc1a3d0fd010714e3783d3f
Uri <Umkil99@gmail.com>
2023-05-18 15:06:11 +0300

    Renamed

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,7 @@
 import "./App.css"
-import LeftBar from './components/LeftBar.js';
-import ContactsBar from './components/ContactsBar.js';
-import ChatBox from './components/ChatBox.js';
+import LeftBar from './ChatScreen/LeftBar.js';
+import ContactsBar from './ChatScreen/ContactsBar.js';
+import ChatBox from './ChatScreen/ChatBox.js';
 
 function Chat() {
   return (
